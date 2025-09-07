15a34409d1936c4c07bc63a4ceec4c6837ff67f7
myssto <pat.lacni@yahoo.com>
2025-09-07 07:28:42 -0500

    eslint react hooks

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import pluginTypescript from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
@@ -15,5 +16,6 @@ export default defineConfig([
   pluginTypescript.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+  pluginReactHooks.configs['recommended-latest'],
   pluginPrettier,
 ]);
